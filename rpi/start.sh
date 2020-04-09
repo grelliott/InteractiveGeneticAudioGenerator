8a7543c7369d7f47e7d0edce9aeec0db0542d463
Grant Elliott <grant@grantelliott.ca>
2020-04-08 21:40:31 -0400

    Fix audio output; it makes noise now!

--- rpi/start.sh
+++ rpi/start.sh
@@ -9,7 +9,7 @@ export DISPLAY=:0
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
 # start jackd
-/usr/local/bin/jackd -P75 -p512 -dalsa -dhw:sndrpihifiberry -r48000 -n2 -D -Phw:sndrpihifiberry,0 -i2 &
+#/usr/local/bin/jackd -ndefault -P75 -p512 -dalsa -dhw:sndrpihifiberry -r48000 -n2 -D -Phw:sndrpihifiberry,0 -i2 &
 
 # start SuperCollider
 echo " " | sclang -D $SUPERCOLLIDER_PATH/$SUPERCOLLIDER_SCRIPT & echo $! > /tmp/sclang.pid
